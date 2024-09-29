e3a8a37b1eb9f4ab06ae310d70fada283c6d2218
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-09-29 17:26:02 -0300

    Csharp

---
+++ Aula07/aula07.cs
@@ -0,0 +1,8 @@
+using System;
+class Principal{
+    static void Main (){
+        const string canal="Isabele Torres";
+        const double pi=3.1415;
+        Console.WriteLine("Canal {0} \nPi: {1}", canal, pi);
+    }
+}
