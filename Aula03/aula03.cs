cd86b2785f9431e55b4b81d34d3980ad7496c43e
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-09-28 22:13:53 -0300

    Csharp

---
+++ Aula03/aula03.cs
@@ -0,0 +1,14 @@
+using System;
+class Aula03{
+    static void Main(){
+        byte n1=10; //entre 0 e 255
+        int numero=0;
+        char letra='c';
+        float valor=9.1f;
+        string nome= "Isabele";
+
+        var aux=10;
+
+        Console.WriteLine("seu nome é: "+ nome +" e tem "+aux+"anos");
+    }
+}
