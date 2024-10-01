ef1841931f00141111dc89e30be6ea395ca31400
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-01 19:44:49 -0300

    Csharp

---
+++ Aula09/aula09.cs
@@ -0,0 +1,11 @@
+//Operações com Bitwise << dobrar  | >> metade
+using System;
+class Aula09{
+    static void Main (){
+        int num=10;
+
+        num=num<<1;
+        
+        Console.WriteLine(num);
+    }
+}
