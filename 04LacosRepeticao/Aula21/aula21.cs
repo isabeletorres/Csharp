ddbf9d16bc3abd34b8cf8144119e2b875dacd00c
isabeletorres <165826756+isabeletorres@users.noreply.github.com>
2024-10-02 16:17:10 -0300

    Csharp

--- 04LacosRepeticao/Aula21/aula21.cs
+++ 04LacosRepeticao/Aula21/aula21.cs
@@ -1,20 +1,23 @@
 using System;
-class Principal{
+class Aula21{
     static void Main (){
         int num=0, i=0;
         
         string senha="123";
         string senharUser=" ";
+        int tentativas=0;
 
         do {
             Console.Clear();
             Console.Write("Digite a senha: ");
             senharUser = Console.ReadLine();
+            tentativas++;
         }while(senha !=senharUser);
 
+        Console.WriteLine("Tentativas {0} ", tentativas);
         Console.Write("Digite um número: ");
         num = int.Parse(Console.ReadLine());
-        
+
         do{
             Console.WriteLine("{0} * {1} = {2}", num, i+1, num*(i+1));
             i++;
---
+++ 04LacosRepeticao/Aula22/aula22.cs
@@ -0,0 +1,15 @@
+//  FOREACH
+using System;
+class Aula22{
+    static void Main (){
+
+        int[] num = new int[5]{11, 22, 33, 44, 55}; 
+
+        for(int i=0; i<num.Length; i++){
+           num[i] =0;
+        }
+        foreach(int n in num){
+            Console.WriteLine(n);
+        }
+    }
+}
